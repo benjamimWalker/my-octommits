fa3c7e5041e737b4a23ecd7a7ec66f152e55b6b8
benjamim <benjamim@bemobile.tech>
2022-12-12 16:51:34 -0300

    refactor: change layouts for login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,36 +1,27 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-<main class="flex items-center justify-center h-screen bg-slate-800">
-    <div class="bg-dark-primary w-96 p-6 rounded-xl shadow-sm">
-        <div class="flex items-center justify-center mb-4">
-            <h1 class="text-white text-xl">
-                {{ $title }}
-            </h1>
-        </div>
+@extends('layout')
 
-        <div class="flex items-center justify-center mb-4">
+@section('content')
+    <main class="flex items-center justify-center h-screen">
+        <div class="bg-dark-primary w-96 p-6 rounded-xl shadow-sm">
+            <div class="flex items-center justify-center mb-4">
+                <h1 class="text-white text-xl">
+                    {{ $title }}
+                </h1>
+            </div>
+
+            <div class="flex items-center justify-center mb-4">
             <span class="text-white">
                 Sign in with your GitHub account
             </span>
+            </div>
+            <div class="flex items-center justify-center mb-4">
+                <a href="{{ route('auth-redirect') }}" class="w-52 inline-flex justify-center py-2 px-2 border border-gray-600 rounded-md shadow-sm bg-white text-sm font-medium text-gray-800 hover:bg-gray-100 dark:bg-gray-50 dark:bg-opacity-5 dark:border dark:border-gray-900 dark:border-opacity-10 dark:text-gray-200 dark:hover:bg-gray-800 duration-300">
+                    <span class="sr-only">Sign in with GitHub</span>
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd"/>
+                    </svg>
+                </a>
+            </div>
         </div>
-        <div class="flex items-center justify-center mb-4">
-            <a href="{{ route('auth-redirect') }}" class="w-52 inline-flex justify-center py-2 px-2 border border-gray-600 rounded-md shadow-sm bg-white text-sm font-medium text-gray-800 hover:bg-gray-100 dark:bg-gray-50 dark:bg-opacity-5 dark:border dark:border-gray-900 dark:border-opacity-10 dark:text-gray-200 dark:hover:bg-gray-800 duration-300">
-                <span class="sr-only">Sign in with GitHub</span>
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd"/>
-                </svg>
-            </a>
-        </div>
-    </div>
-</main>
-</body>
-</html>
+    </main>
+@endsection
