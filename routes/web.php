1cde703a5b456c918a8388ae0f04cb7dccdc9487
benjamim <benjamim@bemobile.tech>
2022-12-12 20:54:38 -0300

    refactor: rearrange routes

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,10 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RepoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])
-    ->middleware('auth')
-    ->name('home');
 
-Route::get('{repo}/history', [RepoController::class, 'repo'])
-    ->middleware('auth')
-    ->name('repo');
-
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('{repo}/history', [RepoController::class, 'repo'])->name('repo');
+});
 
 require __DIR__ . '/auth.php';
