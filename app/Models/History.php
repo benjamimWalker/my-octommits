e3c7b7d8c2cf41e00cae7e7cd5c3ee17a42e7eee
benjamim <benjamim@bemobile.tech>
2022-12-12 20:55:05 -0300

    refactor: remove HasFactory trait from some models

--- app/Models/History.php
+++ app/Models/History.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class History extends Model
 {
-    use HasFactory;
-
     public $timestamps = false;
 
     protected $fillable = [
