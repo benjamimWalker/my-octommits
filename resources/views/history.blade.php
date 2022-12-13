cbbfe82fa0b35df5c34534753aa52b500416a6c0
benjamim <benjamim@bemobile.tech>
2022-12-13 11:42:01 -0300

    fix: open repository page on new tab

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <main>
         <h1 class="text-white text-center text-2xl mt-10">Commits history
-            for <a href="https://github.com/{{ $repo->full_name }}" class="font-bold">{{ $repo->name }}</a> repository</h1>
+            for <a href="https://github.com/{{ $repo->full_name }}" class="font-bold" target="_blank">{{ $repo->name }}</a> repository</h1>
         <x-chart :data="$repo->histories()->select(['date', 'commits'])->orderByDesc('id')->get()"/>
     </main>
 @endsection
