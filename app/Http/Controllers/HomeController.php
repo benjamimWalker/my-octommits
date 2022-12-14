b38f29eca564f81592677f6c28dfcfedf696d25f
benjamim <benjamim@bemobile.tech>
2022-12-13 23:20:27 -0300

    perf: load repos data outside of view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SaveUserRepositories;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $user = auth()->user();
         $title = config('app.name');
+        $repos = $user->repositories()->paginate();
 
-        return view('home', compact('user', 'title'));
+        return view('home', compact('user', 'title', 'repos'));
     }
 }
