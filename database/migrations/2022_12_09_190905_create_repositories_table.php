1724daf75f347c22c2c424ec51155f9c7b7593f1
benjamim <benjamim@bemobile.tech>
2022-12-13 22:00:42 -0300

    fix: remove unique constraint from field on repositories table

--- database/migrations/2022_12_09_190905_create_repositories_table.php
+++ database/migrations/2022_12_09_190905_create_repositories_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('name');
-            $table->string('full_name')->unique();
+            $table->string('full_name');
         });
     }
 
